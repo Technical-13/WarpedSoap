adf7ed4b3a565bd5de4ec153b4ffe29f0d0c9c10
Donald J. Fortier II <Technical_13@yahoo.com>
2024-11-20 13:36:13 -0500

    Update build.js

--- commands/warped/build.js
+++ commands/warped/build.js
@@ -28,62 +28,58 @@ const getPages = ( thisPage ) => {
 }
 const getPageData = async ( doPage ) => {
   await getPages( doPage );
-  new Promise( async ( resolve, reject ) => {
-    try {
-      axios( thisPage ).then( response => {
-      const $ = cheerio.load( response.data );
-      let dataset = $( 'form' )[ 0 ].dataset;
-      let firstImg = $( 'flex-control-thumbs' )[ 0 ].firstChild.firstChild.src.split( '?' )[ 0 ];
-      let hasVariations = ( dataset.product_variations ? true : false );
-      let prodBlurb = $( 'wp-block-post-excerpt__excerpt' )[ 0 ].textContent.trim();
-      let prodCats = Array.from( $( 'taxonomy-product_cat' )[ 0 ].children ).filter( c => c.tagName === 'A' ).map( a => a.textContent );
-      let prodIsSub = ( prodCats.indexOf( 'Subscriptions' ) != -1 ? true : false );
-      let prodDesc = Array.from( Array.from( $( 'tab-description' ).querySelectorAll( 'p' ) ).map( p => p.textContent.trim() ) ).join( '\n' );
-      let prodId = dataset.product_id;
-      let prodInStock = ( $( 'in-stock' ).length ? true : ( prodIsSub || false ) );
-      let prodPrice = parseFloat( $( 'bdi' )[ 0 ].childNodes[ 1 ].textContent );
-      let prodName = $( '[aria-label="Breadcrumb"]' ).lastChild.textContent.split( '/' ).pop().trim();
-      let rating = $( 'strong.rating' )?.textContent;
-      let reviews =  $( 'strong.rating' )?.nextElementSibling.textContent;
-      let prodVars = [];
-      if ( hasVariations && prodIsSub ) { prodVars = JSON.parse( dataset.product_variations ).map( v => v.attributes[ 'attribute_how-often' ] ); }
-      else if ( hasVariations ) { prodVars = JSON.parse( dataset.product_variations ).map( v => v.attributes.attribute_scent ); }
-      let relatedBox = $( 'div[data-block-name="woocommerce/related-products"]' ).firstElementChild;
-      let relatedItems = [];
-      if ( relatedBox ) {
-        relatedItems = Array.from( relatedBox.children[ 1 ].children ).map( ( rp, i ) => {
-          let imgSrc = rp.children[ 0 ].firstElementChild.firstElementChild.src.split( '?' )[ 0 ];
-          let itemName = rp.children[ 1 ].textContent.trim();
-          let itemPrice = rp.children[ 2 ].innerText;
-          let prodId = Array.from( relatedBox.children[ 1 ].children ).map( rp => {
-            return ( rp.children[ 3 ].dataset.wcContext ? JSON.parse( rp.children[ 3 ].dataset.wcContext ).productId : rp.children[ 3 ].dataset.wcContext );
-          } )[ i ];
-          return {
-            prodId: prodId,
-            image: imgSrc,
-            name: itemName,
-            price: ( isNaN( parseFloat( itemPrice ) ) ? itemPrice : parseFloat( itemPrice ) )
-          };
-        } );
-      }
-      let data = {
-        prodId: prodId,
-        blurb: prodBlurb,
-        categories: prodCats,
-        description: prodDesc,
-        image: firstImg,
-        inStock: prodInStock,
-        isSub: prodIsSub,
-        name: prodName,
-        price: prodPrice,
-        rating: ( isNaN( parseFloat( rating ) ) ? rating : parseFloat( rating ) ),
-        reviews: ( isNaN( parseInt( reviews ) ) ? reviews : parseInt( reviews ) ),
-        variations: prodVars,
-        related: relatedItems
-      };
-      resolve( data );
+  axios( thisPage ).then( response => {
+    const $ = cheerio.load( response.data );
+    let dataset = $( 'form' )[ 0 ].dataset;
+    let firstImg = $( 'flex-control-thumbs' )[ 0 ].firstChild.firstChild.src.split( '?' )[ 0 ];
+    let hasVariations = ( dataset.product_variations ? true : false );
+    let prodBlurb = $( 'wp-block-post-excerpt__excerpt' )[ 0 ].textContent.trim();
+    let prodCats = Array.from( $( 'taxonomy-product_cat' )[ 0 ].children ).filter( c => c.tagName === 'A' ).map( a => a.textContent );
+    let prodIsSub = ( prodCats.indexOf( 'Subscriptions' ) != -1 ? true : false );
+    let prodDesc = Array.from( Array.from( $( 'tab-description' ).querySelectorAll( 'p' ) ).map( p => p.textContent.trim() ) ).join( '\n' );
+    let prodId = dataset.product_id;
+    let prodInStock = ( $( 'in-stock' ).length ? true : ( prodIsSub || false ) );
+    let prodPrice = parseFloat( $( 'bdi' )[ 0 ].childNodes[ 1 ].textContent );
+    let prodName = $( '[aria-label="Breadcrumb"]' ).lastChild.textContent.split( '/' ).pop().trim();
+    let rating = $( 'strong.rating' )?.textContent;
+    let reviews =  $( 'strong.rating' )?.nextElementSibling.textContent;
+    let prodVars = [];
+    if ( hasVariations && prodIsSub ) { prodVars = JSON.parse( dataset.product_variations ).map( v => v.attributes[ 'attribute_how-often' ] ); }
+    else if ( hasVariations ) { prodVars = JSON.parse( dataset.product_variations ).map( v => v.attributes.attribute_scent ); }
+    let relatedBox = $( 'div[data-block-name="woocommerce/related-products"]' ).firstElementChild;
+    let relatedItems = [];
+    if ( relatedBox ) {
+      relatedItems = Array.from( relatedBox.children[ 1 ].children ).map( ( rp, i ) => {
+        let imgSrc = rp.children[ 0 ].firstElementChild.firstElementChild.src.split( '?' )[ 0 ];
+        let itemName = rp.children[ 1 ].textContent.trim();
+        let itemPrice = rp.children[ 2 ].innerText;
+        let prodId = Array.from( relatedBox.children[ 1 ].children ).map( rp => {
+          return ( rp.children[ 3 ].dataset.wcContext ? JSON.parse( rp.children[ 3 ].dataset.wcContext ).productId : rp.children[ 3 ].dataset.wcContext );
+        } )[ i ];
+        return {
+          prodId: prodId,
+          image: imgSrc,
+          name: itemName,
+          price: ( isNaN( parseFloat( itemPrice ) ) ? itemPrice : parseFloat( itemPrice ) )
+        };
+      } );
     }
-    catch ( errGetPage ) { reject( errGetPage ); }
+    let data = {
+      prodId: prodId,
+      blurb: prodBlurb,
+      categories: prodCats,
+      description: prodDesc,
+      image: firstImg,
+      inStock: prodInStock,
+      isSub: prodIsSub,
+      name: prodName,
+      price: prodPrice,
+      rating: ( isNaN( parseFloat( rating ) ) ? rating : parseFloat( rating ) ),
+      reviews: ( isNaN( parseInt( reviews ) ) ? reviews : parseInt( reviews ) ),
+      variations: prodVars,
+      related: relatedItems
+    };
+    return data;
   } )
   .then( gotPage => {
     allPages.waiting = allPages.waiting.splice( doPage, 1 );
@@ -91,7 +87,14 @@ const getPageData = async ( doPage ) => {
     console.log( 'gotPage: %o', gotPage );
     //ProductDB.findOneAndUpdate( { name: gotPage.name }, gotPage, { upsert: true } )//Add entry to database
   } )
-  .catch( errPage => { console.error( 'Failed to get page %s: %s', doPage, errPage.stack ); } );
+  .catch( errGetPage => {
+    switch ( errGetPage.status ) {
+      case 404:
+        console.error( 'Page -%s- not found!', thisPage );
+      default:
+        console.error( '%s error attempting to get page -%s- :\n%s', errGetPage.status, doPage, errGetPage.stack );
+    }
+  } );
 };
 
 module.exports = {
